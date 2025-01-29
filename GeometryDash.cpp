a4937a7f3970569fd84512d8f61a3e12d1b43b5b
To-Shii <debbymrtnz@gmail.com>
2025-01-29 15:57:31 +0100

    assets + autre

--- GeometryDash.cpp
+++ GeometryDash.cpp
@@ -21,33 +21,37 @@ void GeometryDash::Start()
 
     //Level::SpawnActor(MeshActor(RectangleShapeData(Vector2f(463.0f, 260.0f) * 2.0f, "background", JPG)));
 
+    MeshActor* _bg = Level::SpawnActor(MeshActor(RectangleShapeData(Vector2f(window.getSize().x, window.getSize().y), "Background", PNG, true)));
+    
     const Vector2f& _floorSize = Vector2f(window.getSize().x, window.getSize().y * 0.2f);
-    MeshActor* floor = Level::SpawnActor(MeshActor(RectangleShapeData(_floorSize, "Background", PNG, true)));
+    MeshActor* _floor = Level::SpawnActor(MeshActor(RectangleShapeData(_floorSize, "Floor", PNG, true)));
     const float _posX = 0.0f;
     const float _posY = window.getSize().y * 0.8f;
-    floor->SetPosition(Vector2f(_posX, _posY));
-    floor->SetTextureRect(IntRect(Vector2i(), Vector2i(512 * 3, 512)));
+    _floor->SetPosition(Vector2f(_posX, _posY));
+    _floor->SetTextureRect(IntRect(Vector2i(), Vector2i(512 * 3, 512)));
 
-    
 
     player = Level::SpawnActor(Player(50.0f, "character"));
     player->SetOriginAtMiddle();
     player->SetPosition(Vector2f(window.getSize().x * 0.5f, window.getSize().y * 0.5f));
 
     const Vector2f& _wallSize = Vector2f(window.getSize().x * 0.2f, window.getSize().y * 1.5f);
-    MeshActor* wall = Level::SpawnActor(MeshActor(RectangleShapeData(_wallSize, "block")));
-    wall->SetPosition(Vector2f(window.getSize().x * 0.8f, 0.0f));
+    MeshActor* _wall = Level::SpawnActor(MeshActor(RectangleShapeData(_wallSize, "block")));
+    _wall->SetPosition(Vector2f(window.getSize().x * 0.8f, 0.0f));
 
-    MeshActor* wall2 = Level::SpawnActor(MeshActor(RectangleShapeData(_wallSize, "block")));
-    wall2->SetPosition(Vector2f(0.0f, 0.0f));
+    MeshActor* _wall2 = Level::SpawnActor(MeshActor(RectangleShapeData(_wallSize, "block")));
+    _wall2->SetPosition(Vector2f(0.0f, 0.0f));
 
-    MeshActor* wall3 = Level::SpawnActor(MeshActor(RectangleShapeData(_floorSize, "rectangle")));
-    wall3->SetPosition(Vector2f(0.0f, 0.0f));
+    MeshActor* _wall3 = Level::SpawnActor(MeshActor(RectangleShapeData(_floorSize, "rectangle")));
+    _wall3->SetPosition(Vector2f(0.0f, 0.0f));
 
-    collidable.push_back(floor);
-    collidable.push_back(wall);
-    collidable.push_back(wall2);
-    collidable.push_back(wall3);
+    MeshActor* _levelComplete = Level::SpawnActor(MeshActor(RectangleShapeData(Vector2f(window.getSize().x /2, window.getSize().y / 2) * 2.0f, "levelComplete", PNG)));
+
+    collidable.push_back(_floor);
+    collidable.push_back(_wall);
+    collidable.push_back(_wall2);
+    collidable.push_back(_wall3);
+
 }
 
 bool GeometryDash::Update()
