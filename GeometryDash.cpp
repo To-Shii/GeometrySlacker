d677ae8b59b8058078212e0007789b3e958aaa31
To-Shii <debbymrtnz@gmail.com>
2025-01-30 13:43:27 +0100

    Merge + texture foncionnelle

--- GeometryDash.cpp
+++ GeometryDash.cpp
@@ -49,6 +49,37 @@ void GeometryDash::Start()
     collidable.push_back(wall);
     collidable.push_back(wall2);
     collidable.push_back(wall3);
+
+    /*MeshActor* _bg = Level::SpawnActor(MeshActor(RectangleShapeData(Vector2f(window.getSize().x, window.getSize().y), "Background", PNG, true)));
+    
+    const Vector2f& _floorSize = Vector2f(window.getSize().x, window.getSize().y * 0.2f);
+    MeshActor* _floor = Level::SpawnActor(MeshActor(RectangleShapeData(_floorSize, "Floor", PNG, true)));
+    const float _posX = 0.0f;
+    const float _posY = window.getSize().y * 0.8f;
+    _floor->SetPosition(Vector2f(_posX, _posY));
+    _floor->SetTextureRect(IntRect(Vector2i(), Vector2i(512 * 3, 512)));
+
+
+    player = Level::SpawnActor(Player(50.0f, "character"));
+    player->SetOriginAtMiddle();
+    player->SetPosition(Vector2f(window.getSize().x * 0.5f, window.getSize().y * 0.5f));
+
+    const Vector2f& _wallSize = Vector2f(window.getSize().x * 0.2f, window.getSize().y * 1.5f);
+    MeshActor* _wall = Level::SpawnActor(MeshActor(RectangleShapeData(_wallSize, "block")));
+    _wall->SetPosition(Vector2f(window.getSize().x * 0.8f, 0.0f));
+
+    MeshActor* _wall2 = Level::SpawnActor(MeshActor(RectangleShapeData(_wallSize, "block")));
+    _wall2->SetPosition(Vector2f(0.0f, 0.0f));
+
+    MeshActor* _wall3 = Level::SpawnActor(MeshActor(RectangleShapeData(_floorSize, "rectangle")));
+    _wall3->SetPosition(Vector2f(0.0f, 0.0f));
+
+    MeshActor* _levelComplete = Level::SpawnActor(MeshActor(RectangleShapeData(Vector2f(window.getSize().x /2, window.getSize().y / 2) * 2.0f, "levelComplete", PNG)));
+
+    collidable.push_back(_floor);
+    collidable.push_back(_wall);
+    collidable.push_back(_wall2);
+    collidable.push_back(_wall3);*/
 }
 
 bool GeometryDash::Update()
